be34d0adee5c6b2f06a5afb963d6f622a40193d5
sebgad <->
2023-11-12 20:08:50 +0100

    initial commit

---
+++ include/file_server.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/param.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#include "esp_err.h"
+#include "esp_log.h"
+
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+#include "esp_http_server.h"
